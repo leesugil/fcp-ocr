14e249a2aefee2fd6be31a3b237c5970c5e93ede
leesugil <sugilmath@gmail.com>
2026-02-25 19:37:56 -0500

    0.0.3

--- src/fcp_ocr/cli.py
+++ src/fcp_ocr/cli.py
@@ -2,7 +2,6 @@
 
 import argparse
 import xml.etree.ElementTree as ET
-from urllib.parse import urlparse, unquote
 import cv2
 
 from . import detect_text
@@ -39,9 +38,11 @@ def main():
 
     # Define possible arguments
     # ex)
-    # fcp-ocr --targetp Save:48,45.5,52,48 --targetp Save:21,91,25,95 --targetp Load:21,91,25,95 --targetp 'Save As:48,52,52,55' --targetp Load:48,57.5,52,61 --targetp Saving:46,47,54,53 --targetp OBS:21,11,27,14 --targetp Stream:68,58,78,61 --targetp Record:68,60,78,64 --targetp Game:13,27,25,33 --targetp Tip:13,27,25,33 --targetp Game:45,25,55,29 --targetp Option:45,32,55,36 --targetp Photo:45,39,55,42 --targetp Mode:45,39,55,42 --targetp Option:46,1,55,5 --skip_seconds=1.5 --ocr_mode=or --affix=ocr_marked_ --debug=0 --optimize=1 <filepath>
+    # fcp-ocr --targetp Save:48,45.5,52,48 --targetp Save:21,91,25,95 --targetp Load:21,91,25,95 --targetp 'Save As:48,52,52,55' --targetp Load:48,57.5,52,61 --targetp Saving:46,47,54,53 --targetp OBS:21,11,27,14 --targetp Stream:68,58,78,61 --targetp Record:68,60,78,64 --targetp Game:13,27,25,33 --targetp Tip:13,27,25,33 --targetp Game:45,25,55,29 --targetp Option:45,32,55,36 --targetp Photo:45,39,55,42 --targetp Mode:45,39,55,42 --targetp Option:46,1,55,5 --skip_seconds=1.5 --ocr_mode=or --affix=ocr_marked_ --optimize <filepath>
     parser = argparse.ArgumentParser(description="Detect texts in video (OCR), place FCP Markers")
     parser.add_argument("fcpxml_filepath", help="Absolute filepath to fcpxml (required)")
+    parser.add_argument("--keyword", type=str, default='silence', help="Keyword to be used in Marker description")
+    parser.add_argument("--event", action="store_true", help="Add this if the fcpxml file is exported from an Event item, not a Project in FCP.")
 
     # video/OCR related
     parser.add_argument("--target", action='append', type=str, help="texts to search in video. TEXT:left,top,right,bottom")
@@ -53,27 +54,17 @@ def main():
     # output
     parser.add_argument("--affix", type=str, default='ocr_marked_', help="affix to modify the output filename")
 
-#   # synched clip
-#   parser.add_argument("--sync", type=int, default=0, help="(experimental) synched clip. 0 or 1")
-
     # debug mode
-    parser.add_argument("--debug", type=int, default=0, help="(experimental) display debug messages. 0 or 1")
+    parser.add_argument("--debug", action='store_true', help="(experimental) display debug messages.")
 
     # optimized mode
-    parser.add_argument("--optimize", type=int, default=1, help="(experimental) improves speed by detecting at most one text per target frame. useful when scanning multiple regions per frame but all you care is whether something was detected or not. 0 or 1")
+    parser.add_argument("--optimize", action='store_true', help="(experimental) improves speed by detecting at most one text per target frame. useful when scanning multiple regions per frame but all you care is whether something was detected or not. 0 or 1")
 
     args = parser.parse_args()
-    #sync = True if args.sync == 1 else False
-    debug = True if args.debug == 1 else False
-    optimize = True if args.optimize == 1 else False
 
     xf = fcpxml_io.clean_filepath(args.fcpxml_filepath)
     vf = fcpxml_io.clean_filepath(fcpxml_io.parse_fcpxml_filepath(xf))
     af = vf
-#   if args.sync == 1:
-#       vf, af = parse_fcpxml_filepath_sync(xf)
-#       vf = clean_filepath(vf)
-#       af = clean_filepath(af)
     print(f"fcpxml file: {xf}")
     print(f"video file: {vf}")
     print(f"audio file: {af}")
@@ -83,10 +74,11 @@ def main():
     width, height = get_resolution(vf)
     if args.targetp:
         target = parse_targetp(args.targetp, width, height)
-    detected_texts = detect_text.detect_texts_from_video(file_path=vf, target=target, skip_frames=args.skip_frames, skip_seconds=args.skip_seconds, mode=args.ocr_mode, debug=debug, optimize=optimize)
+    detected_texts = detect_text.detect_texts_from_video(file_path=vf, target=target, skip_frames=args.skip_frames, skip_seconds=args.skip_seconds, mode=args.ocr_mode, debug=args.debug, optimize=args.optimize)
 
+    tree, root = fcpxml_io.get_fcpxml(xf)
     fps = fcpxml_io.get_fps(root)
-    place_markers.place(filepath=xf, texts=detected_texts, fps=fps)
+    place_markers.place(root=root, texts=detected_texts, fps=fps, keyword=args.keyword, in_event=args.event)
 
     fcpxml_io.save_with_affix(tree=tree, src_filepath=xf, affix=args.affix)
 
--- src/fcp_ocr/place_markers.py
+++ src/fcp_ocr/place_markers.py
@@ -3,19 +3,18 @@ from tqdm import tqdm
 from fcp_io import fcpxml_io
 from fcp_math import arithmetic
 
-def place(filepath: str, texts: list[dict], affix: str, fps):
+def place(root, texts: list[dict], fps: str, keyword: str, in_event: bool=False):
     """
     texts: [{'time': xx.xxx, 'detected': 'ABC XYZ ...'}, {...}, ...]
     """
-    tree, root = fcpxml_io.get_fcpxml(filepath)
-    asset_clip = fcpxml_io.get_event_asset_clip(root)
+    asset_clip = fcpxml_io.get_event_asset_clip(root) if in_event else fcpxml_io.get_spine_asset_clip(root)
 
     # Place OCR Markers
     for i, s in tqdm(enumerate(texts, start=1)):
         start = arithmetic.float2fcpsec(s['time'], fps)
         start_marker = ET.SubElement(asset_clip, "marker")
         start_marker.set("start", start)
-        start_marker.set("value", f"OCR detected {i}: {s['detected']}")
+        start_marker.set("value", f"{keyword} detected {i}: {s['detected']}")
         start_marker.set("duration", fps)
         start_marker.set("completed", "0")
 
